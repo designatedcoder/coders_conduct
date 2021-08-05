a63905ec0617f29d3e618baa0b25912e4c7a6982
designatedcoder <niko@designatedcoder.com>
2021-08-05 03:15:11 -0400

    Created layouts for auth and for app pages

---
+++ client/layouts/appLayout.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex flex-col font-primary min-h-screen">
+        <header class="bg-red-800 text-white py-4">
+            <nav class="container flex justify-between mx-auto w-3/4">
+                <div>Coder's Conduct</div>
+                <div class="space-x-5">
+                    <template v-if="this.$auth.loggedIn">
+                        <span>{{ this.$auth.user.name }}</span>
+                        <NuxtLink to="/dashboard">DASHBOARD</NuxtLink>
+                        <NuxtLink to="#">POSTS</NuxtLink>
+                        <NuxtLink to="#">SETTINGS</NuxtLink>
+                        <a href="#" @click.prevent="logout">LOGOUT</a>
+                    </template>
+                    <template v-else>
+                        <NuxtLink to="/auth/register">REGISTER</NuxtLink>
+                        <NuxtLink to="/auth/login">LOGIN</NuxtLink>
+                    </template>
+                </div>
+            </nav>
+        </header>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            logout() {
+                this.$auth.logout()
+            }
+        }
+    }
+</script>
---
+++ client/layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-cover min-h-screen"  style="background-image: url('https://images.unsplash.com/photo-1581978846964-d3ee3ed0bed1?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTN8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60');">
+        <Nuxt />
+    </div>
+</template>
