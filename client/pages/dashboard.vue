bfde2e3ee58aa28a95dd6aa1f3b5ba7a9ba9defa
designatedcoder <niko@designatedcoder.com>
2021-08-05 03:15:53 -0400

    Create dashboard.vue

---
+++ client/pages/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <main class="container mx-auto my-6 space-y-6 w-3/4">
+            <h1 class="text-2xl font-semibold underline capitalize">Dashboard</h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio dolorem minima quod debitis laborum, voluptatum, rerum quisquam magni, aliquam ut aut dolores porro est! Fugiat quasi unde illo officia incidunt!
+                Dolores, porro? Hic temporibus labore, vitae reprehenderit tempore repellendus nulla aspernatur voluptate fugiat cupiditate ipsum totam molestias dignissimos ratione? Dolor perspiciatis vero quidem illo minima adipisci magnam sed fugiat natus.
+                Quia alias corrupti nobis eveniet incidunt dolorem at assumenda fuga. Itaque modi facere voluptatem sit necessitatibus. Optio ad aliquam excepturi! Quis eligendi sit quia, deserunt quas dolor voluptas accusantium quod?
+                Officiis exercitationem illo labore numquam autem sed eos sit est totam doloribus sequi officia maxime, similique consectetur sapiente aliquam repellendus culpa consequatur adipisci quia minima, ut magnam! Expedita, quasi iste!
+                Totam non quaerat molestiae eos at saepe dolor rem molestias architecto incidunt adipisci deleniti aspernatur sapiente velit aperiam, perferendis officiis voluptate inventore quod libero est qui itaque esse. Architecto, esse.
+            </p>
+        </main>
+    </div>
+</template>
+
+<script>
+    export default {
+        layout: 'appLayout'
+    }
+</script>
